4481039f6994494b03da90f136979293dea33a7b
Rahul <2ar.rahul@gmail.com>
2026-03-04 11:05:59 +0530

    added description to image

--- src/image.schema.ts
+++ src/image.schema.ts
@@ -4,6 +4,7 @@ export const ImageAssetSchema = z.object({
   url: z.string().url(),
   path: z.string(),
   alt: z.string(),
+  desc: z.string().min(1).optional()
 });
 
 export type ImageAsset = z.infer<typeof ImageAssetSchema>;
